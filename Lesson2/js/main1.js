8f7d1ce6fa6971340456eac5b377790f9042d161
tatarintsevaa <tatalexey@gmail.com>
2019-09-27 13:16:17 +0300

    Переделал на ООП.

--- Lesson2/js/main1.js
+++ Lesson2/js/main1.js
@@ -1,14 +1,31 @@
 class ProductList {
+    constructor() {
+        this.items = [];
+    }
 
+    fetchItems() {
+        this.items = [
+            {title: 'Компьютерная мышь', price: 400},
+            {title: 'Жесткий диск SSD 1Tb', price: 10000},
+            {title: 'Материнская плата', price: 4000},
+            {title: 'Видео-карта', price: 15000},
+        ];
+    }
 
+    render() {
+        const block = document.querySelector('.products');
+        this.items.forEach((product) => block.insertAdjacentHTML('afterbegin', new ProductItem(product)
+            .render()))
+    };
 }
 
 class ProductItem {
-    constructor(title, price, image = 'http://via.placeholder.com/150x120') {
-        this.title = title;
-        this.price = price;
+    constructor(product, image = 'http://via.placeholder.com/150x120') {
+        this.title = product.title;
+        this.price = product.price;
         this.image = image;
     }
+
     render() {
         return `<div class="product-item">
         <img src="${this.image}" alt="photo">
@@ -20,4 +37,10 @@ class ProductItem {
      </div>`
     }
 }
+
+const list = new ProductList();
+list.fetchItems();
+list.render();
+
+
 
