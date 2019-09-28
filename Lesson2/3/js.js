08f09c77c22def59de22b7b910fd45796ce4f02f
tatarintsevaa <tatalexey@gmail.com>
2019-09-28 18:36:52 +0300

    Исправил третье задание.

--- Lesson2/3/js.js
+++ Lesson2/3/js.js
@@ -1,15 +1,4 @@
-/*
-### Маленький (50 рублей, 20 калорий).
-### Большой (100 рублей, 40 калорий).
-### Гамбургер может быть с одним из нескольких видов начинок (обязательно):
-### С сыром (+10 рублей, +20 калорий).
-### С салатом (+20 рублей, +5 калорий).
-### С картофелем (+15 рублей, +10 калорий).
-### Дополнительно гамбургер можно посыпать приправой
-(+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий). ### 3Напишите программу,
-рассчитывающую стоимость и калорийность гамбургера. Можно использовать примерную архитектуру класса из методички,
- но можно использовать и свою.
- */
+
 
 class BurgerList {
     constructor(container = '.burgers') {
@@ -115,39 +104,41 @@ class OptionItem {
 const burgerList = new BurgerList();
 const optionList = new OptionList();
 
-const cart = {
-    burgerTotal: [],
-    cartPrice: null,
-    cartCalories: null,
+class Cart {
+    constructor() {
+        this.burgerTotal = [];
+        this.cartPrice = null;
+        this.cartCalories = null;
+    }
     init() {
         this.cartPrice = document.querySelector('#cart-price');
         this.cartCalories = document.querySelector('#cart-calories');
         this.render();
-    },
+    };
     render() {
         this.cartPrice.textContent = this.getBurgerPrice();
         this.cartCalories.textContent = this.getBurgerCalories();
-    },
+    };
     add(burgerName, burgerPrice, burgerCalories) {
         this.burgerTotal.push({name: burgerName, price: burgerPrice, calories: burgerCalories});
         this.render()
-    },
+    };
     dell(burgerName) {
         this.burgerTotal.splice(this.getDellIndex(burgerName), 1);
         this.render();
-    },
+    };
     getBurgerPrice() {
         return this.burgerTotal.reduce((sum, current) => sum + current.price, 0);
-    },
+    };
     getBurgerCalories() {
         return this.burgerTotal.reduce((sum, current) => sum + current.calories, 0);
-    },
+    };
     getDellIndex(burgerName) {
         return this.burgerTotal.findIndex(x => x.name === burgerName)
     }
 
-};
-
+}
+const cart = new Cart();
 window.onload = () => cart.init();
 
 document.querySelectorAll('.burgers__checkbox').forEach(el => {
